7304e4154bb9155e7758d1a2a75ae5e660bd7857
Dmitry A.D <dmitry.dergachev@gmail.com>
2017-01-04 19:52:15 +0000

    init

---
+++ tlgrmBotApi.go
@@ -0,0 +1,37 @@
+package tlgrmBotApi
+
+import (
+//    "encoding/json"; 
+    "net/http"; 
+    "fmt"; 
+    "bytes"
+)
+
+var token string
+
+const urlTemplate string = "https://api.telegram.org/bot%s/%s"
+
+func setToken(extToken string) {
+    token = extToken
+}
+
+func Call(method string, param []byte) (interface{}, bool) {
+    url := fmt.Sprintf(urlTemplate, token, method);
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(param));
+    if err != nil {
+        panic(err)
+    }
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+
+    return nil, true
+}
